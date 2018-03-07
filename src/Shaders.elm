c752b844a1030446e5fbae332058c33aacb658ef
shockham <samuel.hockham@gmail.com>
2018-03-07 09:42:16 +0000

    Make the background from the shader transparent

--- src/Shaders.elm
+++ src/Shaders.elm
@@ -255,7 +255,7 @@ fragmentShader =
 
             if (dist > MAX_DIST - EPSILON) {
                 // Didn't hit anything
-                gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+                gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
                 return;
             }
 
